085b6ba3f4fc3ea76eb32d643091996540b3d268
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-09-09 22:05:42 +0500

    Task3

---
+++ Tyuiu.KorolkovDS.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.KorolkovDS.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.KorolkovDS.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
