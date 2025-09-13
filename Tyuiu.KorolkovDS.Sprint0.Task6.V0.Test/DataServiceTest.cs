ba244f2608d07c795f1d22c86500f343b6953bb4
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-09-13 15:45:36 +0500

    Task6

---
+++ Tyuiu.KorolkovDS.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.KorolkovDS.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KorolkovDS.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+
+        [TestMethod]
+        public void CheckedSubtructionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtructionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+
+        [TestMethod]
+
+        public void CheckedMultiplicationArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+            
+    }
+}
